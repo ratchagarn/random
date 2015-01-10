46091977037e6c7bb214963f666d2852ce72a859
Ratchagarn <ratchagarn@gmail.com>
2015-01-10 20:21:12 +0700

    Add GOD function

--- demo/app.js
+++ demo/app.js
@@ -4,7 +4,7 @@
 
 var util = transit.require('util'),
     groups = transit.require('groups'),
-    random = new ( transit.require('Random') )(),
+    random = transit.require('_random'),
     anim = new ( transit.require('Animation') )();
 
 
@@ -38,6 +38,42 @@ window.anim = anim;
 
 
 /**
+ * `GOD` alway with you !
+ * ------------------------------------------------------------
+ * @name GOD
+ * @param {String} Message for ask GOD for help
+ */
+
+
+window.GOD = function(request_message) {
+
+  var success = true;
+
+  switch (request_message) {
+
+    case 'new world':
+      random.clear();
+      setTimeout(function() {
+        window.location.reload();
+      });
+      break;
+
+
+    default:
+      success = false;
+      console.warn('GOD don\'t hear you !');
+
+  }
+
+  if (success) {
+    console.info('GOD, listen to your request !!!');
+  }
+
+}
+
+
+
+/**
  * ------------------------------------------------------------
  * Bind shortkey
  * ------------------------------------------------------------
@@ -49,7 +85,6 @@ Mousetrap.bind('space', function() {
 });
 
 Mousetrap.bind('r', function() {
-  console.log('PRESS `R`')
   anim.go();
 });
 
